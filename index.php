7b7f764598b07378f72c702654ae11279a5746d5
Andressa <andressa.sena@mutua.com.br>
2024-03-08 14:49:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Projeto_LANU">
     <meta name="author" content="Andressa Monteiro">
     <meta name="keywords" content="html, css, php">
@@ -14,13 +14,11 @@
 
     <title>Mútua de Estagiário</title>
 
-     <!--Google Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-
 </head>
 <body>
+    <div class="container">
+        <h1>Mútua de Estagiário</h1>
+    </div>
     
 </body>
 </html>
